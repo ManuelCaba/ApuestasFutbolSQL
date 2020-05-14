57391cdc3fd6670a144a0361c17942b3ad263712
ManuelCaballero <emanenxd@gmail.com>
2020-05-14 20:39:34 +0200

    Triggersitos

--- ApuestasFutbol.sql
+++ ApuestasFutbol.sql
@@ -4,6 +4,10 @@ GO
 Use ApuestasFutbol
 GO
 
+
+SET NOCOUNT ON
+GO
+
 Create Table Usuarios (
 
 	Nick Varchar(20) NOT NULL,
@@ -116,75 +120,62 @@ Create Table GanadoresPartidos (
 )
 GO
 
---Funciones Clasificacion
-CREATE TYPE Partidos 
-   AS TABLE
-      (
-		ID Int,
-		ELocal Char(4),
-		EVisitante Char(4),
-		GolesLocal TinyInt,
-		GolesVisitante TinyInt,
-		Finalizado Bit,
-		Fecha SmallDateTime
-	  )
-GO
-
-CREATE OR ALTER FUNCTION PartidosGanadosLocal (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+--Vistas Clasificacion
+CREATE OR ALTER VIEW PartidosGanadosLocal AS
 (
-	SELECT ELocal AS ID, COUNT(Elocal) PartidosGanadosLocal FROM @Partidos
+	SELECT ELocal AS ID, COUNT(Elocal) PartidosGanadosLocal FROM Partidos
 	WHERE GolesLocal > GolesVisitante
 	GROUP BY ELocal		
 )
 GO
 
-CREATE OR ALTER FUNCTION PartidosGanadosVisitante (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW PartidosGanadosVisitante AS
 (
-	SELECT EVisitante AS ID, COUNT(EVisitante) PartidosGanadosVisitante FROM @Partidos
+	SELECT EVisitante AS ID, COUNT(EVisitante) PartidosGanadosVisitante FROM Partidos
 	WHERE GolesVisitante > GolesLocal
 	GROUP BY EVisitante
 )
 GO
 
-CREATE OR ALTER FUNCTION PartidosEmpatados (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW PartidosEmpatados AS
 (
 	SELECT E.ID, COUNT(*) AS PartidosEmpatados FROM Equipos AS E 
-	INNER JOIN @Partidos AS P ON (E.ID = P.ELocal OR E.ID = P.EVisitante) AND P.GolesLocal = P.GolesVisitante
+	INNER JOIN Partidos AS P ON (E.ID = P.ELocal OR E.ID = P.EVisitante) AND P.GolesLocal = P.GolesVisitante
 	WHERE P.Finalizado = 1
 	GROUP BY E.ID
 )
 GO
 
-CREATE OR ALTER FUNCTION PartidosTotales (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW PartidosTotales AS
 (
 	SELECT E.ID, COUNT(*) AS PartidosTotales FROM Equipos AS E
-	INNER JOIN @Partidos AS P ON E.ID = P.ELocal OR E.ID = P.EVisitante
+	INNER JOIN Partidos AS P ON E.ID = P.ELocal OR E.ID = P.EVisitante
 	WHERE P.Finalizado = 1
 	GROUP BY E.ID
 )
 GO
 
 
-CREATE OR ALTER FUNCTION GolesLocales (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW GolesLocales AS
 (
-	SELECT ELocal AS ID, SUM(GolesLocal) AS GolesLocal FROM @Partidos
+	SELECT ELocal AS ID, SUM(GolesLocal) AS GolesLocal FROM Partidos
 	GROUP BY ELocal
 )
 GO
 
-CREATE OR ALTER FUNCTION GolesVisitante (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW GolesVisitante AS
 (
-	SELECT EVisitante AS ID, SUM(GolesVisitante) AS GolesVisitante FROM @Partidos
+	SELECT EVisitante AS ID, SUM(GolesVisitante) AS GolesVisitante FROM Partidos
 	GROUP BY EVisitante
 )
 GO
 
-CREATE OR ALTER FUNCTION GolesEnContra (@Partidos Partidos READONLY) RETURNS TABLE AS RETURN
+CREATE OR ALTER VIEW GolesEnContra AS
 (
 	SELECT E.ID, SUM(CASE WHEN P.ELocal <> E.ID THEN GolesLocal
 									ELSE GolesVisitante
 					 END) AS GolesEnContra FROM Equipos AS E 
-	INNER JOIN @Partidos AS P ON E.ID = P.ELocal OR E.ID = P.EVisitante
+	INNER JOIN Partidos AS P ON E.ID = P.ELocal OR E.ID = P.EVisitante
 	GROUP BY E.ID
 )
 GO
@@ -194,26 +185,23 @@ GO
 CREATE OR ALTER TRIGGER ActualizarClasificacion ON Partidos AFTER INSERT,UPDATE
 AS
 	BEGIN
+		
+		SET NOCOUNT ON
 
-	DECLARE @Partidos AS Partidos
+		DELETE FROM Clasificaciones
 
-	INSERT @Partidos (ID,ELocal,EVisitante,GolesLocal,GolesVisitante,Finalizado,Fecha)
-	SELECT * FROM Partidos
+		DBCC CHECKIDENT (Clasificaciones, RESEED,0)
 
-	DELETE FROM Clasificaciones
-
-	DBCC CHECKIDENT (Clasificaciones, RESEED,0)
-
-	INSERT Clasificaciones
-	SELECT E.ID, E.Nombre, ISNULL(PT.PartidosTotales,0), (ISNULL(PGL.PartidosGanadosLocal,0) + ISNULL(PGV.PartidosGanadosVisitante,0)) AS PartidosGanados, ISNULL(PE.PartidosEmpatados,0), (ISNULL(GL.GolesLocal,0) + ISNULL(GV.GolesVisitante,0)) AS GolesFavor, ISNULL(GE.GolesEnContra,0) FROM Equipos AS E 
-	LEFT JOIN PartidosGanadosLocal(@Partidos) AS PGL ON E.ID = PGL.ID
-	FULL JOIN PartidosGanadosVisitante(@Partidos) AS PGV ON E.ID = PGV.ID
-	FULL JOIN PartidosEmpatados(@Partidos) AS PE ON E.ID = PE.ID
-	FULL JOIN PartidosTotales(@Partidos) AS PT ON E.ID = PT.ID
-	FULL JOIN GolesLocales(@Partidos) AS GL ON E.ID = GL.ID
-	FULL JOIN GolesVisitante(@Partidos) AS GV ON E.ID = GV.ID
-	FULL JOIN GolesEnContra(@Partidos) AS GE ON E.ID = GE.ID
-	ORDER BY (((PGL.PartidosGanadosLocal + PGV.PartidosGanadosVisitante) * 3) + PE.PartidosEmpatados) DESC, (GV.GolesVisitante + GL.GolesLocal - GE.GolesEnContra) DESC, (GV.GolesVisitante + GL.GolesLocal) DESC, PGV.PartidosGanadosVisitante DESC, GV.GolesVisitante DESC
+		INSERT Clasificaciones
+		SELECT E.ID, E.Nombre, ISNULL(PT.PartidosTotales,0), (ISNULL(PGL.PartidosGanadosLocal,0) + ISNULL(PGV.PartidosGanadosVisitante,0)) AS PartidosGanados, ISNULL(PE.PartidosEmpatados,0), (ISNULL(GL.GolesLocal,0) + ISNULL(GV.GolesVisitante,0)) AS GolesFavor, ISNULL(GE.GolesEnContra,0) FROM Equipos AS E 
+		LEFT JOIN PartidosGanadosLocal AS PGL ON E.ID = PGL.ID
+		FULL JOIN PartidosGanadosVisitante AS PGV ON E.ID = PGV.ID
+		FULL JOIN PartidosEmpatados AS PE ON E.ID = PE.ID
+		FULL JOIN PartidosTotales AS PT ON E.ID = PT.ID
+		FULL JOIN GolesLocales AS GL ON E.ID = GL.ID
+		FULL JOIN GolesVisitante AS GV ON E.ID = GV.ID
+		FULL JOIN GolesEnContra AS GE ON E.ID = GE.ID
+		ORDER BY (((PGL.PartidosGanadosLocal + PGV.PartidosGanadosVisitante) * 3) + PE.PartidosEmpatados) DESC, (GV.GolesVisitante + GL.GolesLocal - GE.GolesEnContra) DESC, (GV.GolesVisitante + GL.GolesLocal) DESC, PGV.PartidosGanadosVisitante DESC, GV.GolesVisitante DESC
 	END
 GO
 
@@ -245,42 +233,96 @@ AS
 	END
 GO
 
--- Equipos participantes 
-INSERT INTO Equipos (ID,Nombre,Ciudad,Pais)
-     VALUES ('RBET','Real Betis','Sevilla','España'),('LIVL','Liverpool FC','Liverpool','Reino Unido'),('ESRO','Estrella Roja','Belgrado','Serbia'),
-	 ('AJAX','Ajax','Amsterdam','Holanda'),('MANC','Manchester City','Manchester','Reino Unido'),('ARAR','Ararat','Erevan','Armenia'),
-	 ('BODO','Borussia Dortmund','Dortmund','Alemania'),('BARC','FC Barcelona','Barcelona','España'),('PASG','Paris Saint Germain','Paris','Francia'),
-	 ('OLYM','Olympiacos','Atenas','Grecia'),('MANU','Manchester United','Manchester','Reino Unido'),('OLYL','Olympique de Lion','Lion','Francia'),
-	 ('INTM','Inter','Milan','Italia'),('BENF','Benfica','Lisboa','Portugal'),('BAYM','Bayern','Munich','Alemania'),('JUVT','Juventus','Turin','Italia'),
-	 ('ZENR','Zenit','San Petesburgo','Rusia'), ('RMAD','Real Madrid','Madrid','España')
+CREATE OR ALTER TRIGGER ActualizarApuesta ON Apuestas FOR UPDATE
+AS
+	BEGIN
+		IF(update(DineroApostado))
+			BEGIN
+				RAISERROR('No se puede actualizar una apuesta.',10,1)
+				ROLLBACK Transaction
+			END
+	END
 GO
 
+CREATE OR ALTER TRIGGER ActualizarHandicap ON Handicaps FOR UPDATE
+AS
+	BEGIN
+		IF(update(Handicap))
+			BEGIN
+				RAISERROR('No se puede actualizar una apuesta.',10,1)
+				ROLLBACK Transaction
+			END
+	END
+GO
+
+CREATE OR ALTER TRIGGER ActualizarOverUnder ON OversUnders FOR UPDATE
+AS
+	BEGIN
+		IF(update([Over/Under]) OR update(Numero))
+			BEGIN
+				RAISERROR('No se puede actualizar una apuesta.',10,1)
+				ROLLBACK Transaction
+			END
+	END
+GO
+
+CREATE OR ALTER TRIGGER ActualizarGanadorPartido ON GanadoresPartidos FOR UPDATE
+AS
+	BEGIN
+		IF(update(Resultado))
+			BEGIN
+				RAISERROR('No se puede actualizar una apuesta.',10,1)
+				ROLLBACK Transaction
+			END
+	END
+GO
+
+-- Poblamos la tabla equipos 
+CREATE OR ALTER PROCEDURE PoblarEquipos
+AS
+	BEGIN
+		INSERT INTO Equipos (ID,Nombre,Ciudad,Pais)
+			VALUES ('RBET','Real Betis','Sevilla','España'),('LIVL','Liverpool FC','Liverpool','Reino Unido'),('GTFE','Getafe CF','Getafe','España'),
+			('AJAX','Ajax','Amsterdam','Holanda'),('MANC','Manchester City','Manchester','Reino Unido'),('OPRT','FC Oporto','Oporto','Portugal'),
+			('BODO','Borussia Dortmund','Dortmund','Alemania'),('BARC','FC Barcelona','Barcelona','España'),('PASG','Paris Saint Germain','Paris','Francia'),
+			('ROMA','AS Roma','Roma','Italia'),('MANU','Manchester United','Manchester','Reino Unido'),('OLYL','Olympique de Lion','Lion','Francia'),
+			('INTM','Inter','Milan','Italia'),('BENF','Benfica','Lisboa','Portugal'),('BAYM','Bayern','Munich','Alemania'),('JUVT','Juventus','Turin','Italia'),
+			('CSKM','PFC CSKA Moscu','Moscú','Rusia'), ('RMAD','Real Madrid','Madrid','España')
+	END
+GO
+
+EXECUTE PoblarEquipos
+GO
 -- Poblamos la tabla Partidos
 
-Insert Into Partidos (ELocal ,EVisitante)
-SELECT L.ID, V.ID FROM Equipos AS L CROSS JOIN Equipos AS V Where L.ID <> V.ID
+CREATE OR ALTER PROCEDURE PoblarPartidos 
+AS
+	BEGIN
+		Insert Into Partidos (ELocal ,EVisitante)
+		SELECT L.ID, V.ID FROM Equipos AS L CROSS JOIN Equipos AS V Where L.ID <> V.ID
+		
+		DECLARE @GolesL TinyInt, @GolesV TinyInt, @Partido SmallInt
+		DECLARE CPartidos CURSOR FOR Select ID From Partidos
+		Open Cpartidos
+		Fetch Next FROM Cpartidos INTO @Partido
+		While @@FETCH_STATUS = 0
+		Begin
+			If @Partido % 15 <> 0
+			Begin
+				SET @GolesL = Floor(rand()*4)
+				SET @GolesV = Floor(rand()*3)
+				Update Partidos Set GolesLocal = @GolesL, GolesVisitante = @GolesV, Finalizado = 1
+					Where Current Of Cpartidos
+			End -- If
+			Fetch Next FROM Cpartidos INTO @Partido
+		End -- While
+		Close Cpartidos
+		Deallocate CPartidos
+	END
 GO
-DECLARE @GolesL TinyInt, @GolesV TinyInt, @Partido SmallInt
-DECLARE CPartidos CURSOR FOR Select ID From Partidos
-Open Cpartidos
-Fetch Next FROM Cpartidos INTO @Partido
-While @@FETCH_STATUS = 0
-Begin
-	If @Partido % 15 <> 0
-	Begin
-		SET @GolesL = Floor(rand()*4)
-		SET @GolesV = Floor(rand()*3)
-		Update Partidos Set GolesLocal = @GolesL, GolesVisitante = @GolesV, Finalizado = 1
-			Where Current Of Cpartidos
-	End -- If
-	Fetch Next FROM Cpartidos INTO @Partido
-End -- While
-Close Cpartidos
-Deallocate CPartidos
 
--- Mucho Betis!
--- El factor Villamarin
-Update Partidos Set GolesLocal = GolesLocal + 1
-	Where ELocal ='RBET'
+EXECUTE PoblarPartidos
 
-Select * From Partidos
+-- Poblamos la tabla Usuarios
+
+--CREATE PROCEDURE 
