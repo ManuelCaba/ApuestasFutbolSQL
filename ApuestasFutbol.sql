60afa568296748e7c36d1580bcc0cfc2706c637b
Alex1sK1ng <alexgatabarros@gmail.com>
2020-06-05 12:01:57 +0200

    Update ApuestasFutbol.sql

--- ApuestasFutbol.sql
+++ ApuestasFutbol.sql
@@ -492,7 +492,98 @@ AS
 	END
 GO
 
+--Por último poblamos la tabla Apuestas
+
+--BEGIN TRANSACTION
+
+CREATE OR ALTER PROCEDURE PoblarApuestas
+AS	
+	BEGIN 
+
+		DECLARE @Apuestas Int = Floor(rand()*400) + 600
+		DECLARE @DineroApostado Int
+		DECLARE @Partido Int
+		DECLARE @ProbabilidadComprobada TinyInt
+		DECLARE @Cont SmallInt = 1
+		DECLARE @Comprobada Bit
+		DECLARE @NickUsuario Varchar(20)
+		DECLARE @TipoApuesta TinyInt
+		DECLARE @Resultado TinyInt
+		DECLARE @ResultadoAleatorio DECIMAL(2,1)
+		DECLARE @Apuesta SmallInt
+
+
+		WHILE @Cont <= @Apuestas
+			BEGIN
+		
+				SET @DineroApostado = Floor(rand() * 150) + 50
+				SET @ProbabilidadComprobada = Floor(rand() * 9) + 1
+				SET @TipoApuesta = Floor(rand() * 3) + 1
+
+				SELECT TOP 1 @NickUsuario = Nick FROM Usuarios
+				ORDER BY NEWID()
+
+				SELECT TOP 1 @Partido = ID FROM Partidos
+				ORDER BY NEWID()
+
+				IF @ProbabilidadComprobada > 4
+					SET @Comprobada = 0
+				ELSE
+					SET @Comprobada = 1
+		
+				IF(NOT EXISTS (SELECT * FROM Apuestas WHERE NickUsuario = @NickUsuario AND IDPartido = @Partido))
+					BEGIN
+						INSERT Apuestas (DineroApostado, IDPartido, NickUsuario, Comprobada)
+						SELECT @DineroApostado,@Partido, @NickUsuario, @Comprobada
+
+						SET @Apuesta = @@IDENTITY
+
+						IF @TipoApuesta = 1
+							BEGIN
+								SET @Resultado = Floor(rand() * 3) + 1
+
+								INSERT GanadoresPartidos
+								VALUES(@Apuesta, (
+													SELECT CASE @Resultado
+															WHEN 1 THEN '1'
+															WHEN 2 THEN 'X'
+															ELSE '2'
+														   END
+												 ))
+							END
+						ELSE IF @TipoApuesta = 2
+							BEGIN
+								WHILE @Resultado = 0
+									SET @Resultado = Floor(rand() * 7) - 3
+
+								INSERT Handicaps
+								VALUES(@Apuesta, @Resultado)
+							END
+						ELSE
+							BEGIN
+								SET @Resultado = Floor(rand() * 2)
+								SET @ResultadoAleatorio = Floor(rand() * 6) + 0.5
+
+								INSERT OversUnders
+								VALUES(@Apuesta, @Resultado, @ResultadoAleatorio)
+							END
+
+						SET @Cont += 1
+					END
+			
+
+			END
+
+	END
+
+GO
+
+--ROLLBACK
+
 EXECUTE PoblarUsuarios
+GO
+
+
 -- Poblamos la tabla Apuestas
 --GO
 --CREATE OR ALTER PROCEDURE PoblarApuestas
